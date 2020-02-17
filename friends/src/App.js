eb7bc3474a9e5ee8d6ff4aefcdb9196d94fa8363
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-02-16 22:32:17 -0500

    added a signin page

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import { Link, Route } from 'react-router-dom'
+
+import Signin from './components/Signin'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/signin">Signin</Link>
+        </nav>
+        <Route exact path="/signin" component={Signin} />
       </header>
     </div>
   );
